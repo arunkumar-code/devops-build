07b7972f05059feaa01b23f018a69120097205a8
arunkumar-code <56575130+arunkumar-code@users.noreply.github.com>
2024-12-26 14:05:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_EADWHoJ3ZHIAazRONGu5WkNt0EY
-    docker tag test ar8888/dev
-    docker push ar8888/dev
+    docker login -u arun05docker -p dckr_pat_KJmqw8P99rDytytfrgDCg96OC8E
+    docker tag test arun05docker/dev
+    docker push arun05docker/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_EADWHoJ3ZHIAazRONGu5WkNt0EY
-    docker tag test ar8888/prod 
-    docker push ar8888/prod
+    docker login -u arun05docker -p dckr_pat_KJmqw8P99rDytytfrgDCg96OC8E
+    docker tag test arun05docker/prod 
+    docker push arun05docker/prod
 fi
